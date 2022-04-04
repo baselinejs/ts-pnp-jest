3c5efb4634895813661b8606cdba682d59d462d7
Dean Hallman <dean@foresight.dev>
2022-04-04 10:54:21 -0400

    build: ensure eslint and prettier don't overlap in their purpose

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,15 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module' // Allows for the use of imports
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier' // disable all formatting related linting, since prettier will handle that
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    'prettier/prettier': 2
   }
 };
